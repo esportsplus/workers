5d75598e54960841c2750c092e9352f832286390
Israel Casillas Jr <ICJR@users.noreply.github.com>
2022-12-02 06:37:08 -0800

    fix: type inference

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,11 @@
 type Infer<T> =
-    T extends (...args: any) => any
-        ? () => Promise< ReturnType<T> >
-        : T extends object
-            ? { [K in keyof T]: Infer<T[K]> }
-            : () => Promise<T>;
+    T extends (...args: any[]) => Promise<any>
+        ? T
+        : T extends (...args: any[]) => any
+            ? () => Promise< ReturnType<T> >
+            : T extends Record<string, any>
+                ? { [K in keyof T]: Infer<T[K]> }
+                : never;
 
 
 export { Infer };
